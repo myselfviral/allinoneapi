a0442e66768d2119a77b08cc60031ac7fcd529a2
ViralP17 <viral.patel@varianceinfotech.com>
2020-09-14 16:00:24 +0530

    remove port

--- example.js
+++ example.js
@@ -69,7 +69,7 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 //app.listen(port);
-console.log('Magic happens on port ' + port);
+//console.log('Magic happens on port ' + port);
 
 typeorm.createConnection({
 	type: "mysql",
